e876fc4d38bcb0406f380a917ac439d6c5235dc9
David Russell <davidrussell327@gmail.com>
2023-04-28 13:47:50 -0400

    Geotag and add granular timing information

--- saving/images_from_dji_video.py
+++ saving/images_from_dji_video.py
@@ -4,12 +4,17 @@ import datetime
 import time
 import os
 import json
+from gps_tag_images import set_gps_location
+from pathlib import Path
 
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Extract images from a DJI video.")
     parser.add_argument("video_file", help="Path to video file.")
     parser.add_argument("output_dir", help="Output directory.")
+    parser.add_argument("--delta", default=0.5, help="step between images", type=float)
+    parser.add_argument("--start-time", type=float)
+    parser.add_argument("--stop-time", type=float)
     args = parser.parse_args()
     return args
 
@@ -36,13 +41,15 @@ def parse_subtitles(video_file):
     return time_pos_dict
 
 
-def main(video_file, output_dir, delta=0.5):
+def main(video_file, output_dir, delta=0.5, start_time=None, stop_time=None):
+
+    video_stem = Path(video_file).stem
+    output_dir = str(Path(output_dir, video_stem))
     # Find the subtitles file
     # Open the video
     # Iterate through the video and save each frame
 
     gps_dict = parse_subtitles(video_file)
-    cap = cv2.VideoCapture(video_file)
 
     os.makedirs(output_dir, exist_ok=True)
     gps_file = output_dir + "/gps.json"
@@ -50,16 +57,31 @@ def main(video_file, output_dir, delta=0.5):
         gps_file_h.write(json.dumps(gps_dict))
 
     last_time = 0
+
+    cap = cv2.VideoCapture(video_file)
+
     for time, pos in gps_dict.items():
-        if time - last_time < delta:
-            continue
+        delta_time = time - last_time
 
         ret, frame = cap.read()
         if not ret:
             return
-        cv2.imwrite(f"{output_dir}/time_{time}.jpg", frame)
+
+        if delta_time < delta:
+            continue
+
+        if start_time is not None and time < start_time:
+            continue
+
+        if stop_time is not None and time < stop_time:
+            return 
+         
+        
+        filename = f"{output_dir}/time_{time:06f}.jpg"
+        cv2.imwrite(filename, frame)
+        set_gps_location(filename, lat=pos["lat"], lng=pos["lon"], altitude=pos["alt"])
         last_time = time
 
 if __name__ == "__main__":
     args = parse_args()
-    main(args.video_file, args.output_dir)
+    main(args.video_file, args.output_dir, delta=args.delta, start_time=args.start_time, stop_time=args.stop_time)
